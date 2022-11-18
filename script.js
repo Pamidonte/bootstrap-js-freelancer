bd40ec3587b880a5d53017a227914d09eaa88b2f
Pamidonte <francescomarrone8@gmail.com>
2022-11-18 16:03:33 +0100

    se inserisco stringa vuota non esce "lo sc nn vld"

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-function submitForm(event){
+function submitForm(event) {
     event.preventDefault();
     calcoloprezzo();
 }
 
 
-function sconto(prezzofinale){
-    let codiceSconto=document.getElementById("inputdiscountcode").value;
+function sconto(prezzofinale) {
+    let codiceSconto = document.getElementById("inputdiscountcode").value;
     console.log(codiceSconto);
-    let scontoDaSottrarre= 0;
-    let scontononvalido=document.getElementById("scontononvalido");
-    if (codiceSconto.toString() == "YHDNU32" || codiceSconto.toString() == "JANJC63" || codiceSconto.toString() == "PWKCN25" || codiceSconto.toString() == "SJDPO96" || codiceSconto.toString() == "POCIE24"){
-        scontoDaSottrarre= (prezzofinale *25 ) /100;
+    let scontoDaSottrarre = 0;
+    let scontononvalido = document.getElementById("scontononvalido");
+    if (codiceSconto.toString() == "YHDNU32" || codiceSconto.toString() == "JANJC63" || codiceSconto.toString() == "PWKCN25" || codiceSconto.toString() == "SJDPO96" || codiceSconto.toString() == "POCIE24") {
+        scontoDaSottrarre = (prezzofinale * 25) / 100;
         console.log(scontoDaSottrarre);
-        scontononvalido.innerHTML="";
-        
-        } else{
-            scontononvalido.innerHTML="lo sconto inserito non è valido";
+        scontononvalido.innerHTML = "";
 
+    } else {
+        if (codiceSconto.toString() != "") {
+            scontononvalido.innerHTML = "lo sconto inserito non è valido";
         }
-        let prezzototale= document.getElementById("prezzototale");
-    prezzototale.innerHTML= Math.floor(prezzofinale-scontoDaSottrarre).toString();
-    
+
+    }
+    let prezzototale = document.getElementById("prezzototale");
+    prezzototale.innerHTML = Math.floor(prezzofinale - scontoDaSottrarre).toString();
+
 }
 
 
 
 function calcoloprezzo() {
-    let tipodilavoro= document.getElementById("inputwork");
-    let prezzolavoro= tipodilavoro.value;
+    let tipodilavoro = document.getElementById("inputwork");
+    let prezzolavoro = tipodilavoro.value;
     console.log(prezzolavoro);
-    let oretotali= document.getElementById("inputhours").value;
+    let oretotali = document.getElementById("inputhours").value;
     console.log(oretotali);
-    let prezzofinale= prezzolavoro * oretotali;
+    let prezzofinale = prezzolavoro * oretotali;
     console.log(prezzofinale);
     sconto(prezzofinale);
 }
